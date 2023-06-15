87931ecfb53c3da541d269ee98a4cc5508b2c7d0
ArchLeaders <archparliament@gmail.com>
2023-06-15 02:13:34 -0700

    Define IConfigModule and define abstract implementations

---
+++ src/ConfigFactory.Core/IConfigModule.cs
@@ -0,0 +1,10 @@
+﻿namespace ConfigFactory.Core;
+
+/// <summary>
+/// Defines a configuration (settings) extension
+/// </summary>
+public interface IConfigModule
+{
+    public IConfigModule Load();
+    public void Save();
+}
