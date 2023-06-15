f312210bba770360551ded7336550f749cb1ddfc
ArchLeaders <archparliament@gmail.com>
2023-06-15 02:15:27 -0700

    Create ConfigItem templated control

---
+++ src/ConfigFactory.Avalonia/Controls/ConfigItem.axaml.cs
@@ -0,0 +1,48 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace ConfigFactory.Avalonia.Controls;
+
+public class ConfigItem : ContentControl
+{
+    public static readonly StyledProperty<string> HeaderProperty =
+		AvaloniaProperty.Register<ConfigItem, string>(nameof(Header));
+
+    public static readonly StyledProperty<string> DescriptionProperty =
+		AvaloniaProperty.Register<ConfigItem, string>(nameof(Description));
+
+    public static readonly StyledProperty<BoxShadows> BoxShadowProperty =
+		AvaloniaProperty.Register<ConfigItem, BoxShadows>(nameof(BoxShadow));
+
+    public static readonly StyledProperty<GridLength> ContentColumnProperty =
+		AvaloniaProperty.Register<ConfigItem, GridLength>(nameof(ContentColumn), new GridLength(1, GridUnitType.Star));
+
+    public static readonly StyledProperty<IBrush> ValidationBrushProperty =
+		AvaloniaProperty.Register<ConfigItem, IBrush>(nameof(ValidationBrush), defaultValue: Brushes.Transparent);
+
+    public string Header {
+		get => GetValue(HeaderProperty);
+		set => SetValue(HeaderProperty, value);
+	}
+
+	public string Description {
+		get => GetValue(DescriptionProperty);
+		set => SetValue(DescriptionProperty, value);
+	}
+
+	public BoxShadows BoxShadow {
+		get => GetValue(BoxShadowProperty);
+		set => SetValue(BoxShadowProperty, value);
+	}
+
+	public GridLength ContentColumn {
+		get => GetValue(ContentColumnProperty);
+		set => SetValue(ContentColumnProperty, value);
+	}
+
+	public IBrush ValidationBrush {
+		get => GetValue(ValidationBrushProperty);
+		set => SetValue(ValidationBrushProperty, value);
+	}
+}
