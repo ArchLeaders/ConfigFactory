c3c5f1ce03dc736d851f8ab22156633718fd2631
ArchLeaders <archparliament@gmail.com>
2023-06-16 14:47:23 -0700

    Define IControlBuilder and abstract impl

---
+++ src/ConfigFactory.Avalonia/Generics/ControlBuilder.cs
@@ -0,0 +1,11 @@
+﻿using ConfigFactory.Core;
+
+namespace ConfigFactory.Avalonia.Generics;
+
+public abstract class ControlBuilder<T> : IControlBuilder where T : ControlBuilder<T>, new()
+{
+    public static T Shared { get; } = new();
+
+    public abstract object? Build(IConfigModule context, string propertyName);
+    public abstract bool IsValid(object? value);
+}
---
+++ src/ConfigFactory.Avalonia/Generics/IControlBuilder.cs
@@ -0,0 +1,9 @@
+﻿using ConfigFactory.Core;
+
+namespace ConfigFactory.Avalonia.Generics;
+
+public interface IControlBuilder
+{
+    public object? Build(IConfigModule context, string propertyName);
+    public bool IsValid(object? value);
+}
