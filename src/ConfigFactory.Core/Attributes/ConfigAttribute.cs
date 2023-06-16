0173194493f5ff49c2860e965c8ffa3ce49e70fd
ArchLeaders <archparliament@gmail.com>
2023-06-16 14:49:06 -0700

    Add Category and Group to ConfigAttribute

--- src/ConfigFactory.Core/Attributes/ConfigAttribute.cs
+++ src/ConfigFactory.Core/Attributes/ConfigAttribute.cs
@@ -5,4 +5,6 @@ public class ConfigAttribute : Attribute
 {
     public required string Header { get; set; }
     public required string Description { get; set; }
+    public string Category { get; set; } = "General";
+    public string Group { get; set; } = "Common";
 }
