8bcab5bd725439e86ea334be15b3d41dc9679c58
ArchLeaders <archparliament@gmail.com>
2023-06-16 14:52:38 -0700

    Impl extended attributes (Text, Dropdown)

---
+++ src/ConfigFactory.Core/Attributes/BrowserConfigAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace ConfigFactory.Core.Attributes;
+
+public enum BrowserMode
+{
+    None,
+    OpenFile,
+    OpenFolder,
+    SaveFile
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class BrowserConfigAttribute : Attribute
+{
+    public BrowserMode BrowserMode { get; set; }
+}
---
+++ src/ConfigFactory.Core/Attributes/DropdownConfigAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace ConfigFactory.Core.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class DropdownConfigAttribute : Attribute
+{
+    public string[] ItemsSource { get; set; } = Array.Empty<string>();
+
+    /// <summary>
+    /// The name of the function or method in the owning
+    /// class to return an <see cref="IEnumerable{T}"/> of source elements
+    /// </summary>
+    public string? RuntimeItemsSourceMethodName { get; set; }
+
+    public DropdownConfigAttribute() { }
+    public DropdownConfigAttribute(params string[] items)
+    {
+        ItemsSource = items;
+    }
+}
