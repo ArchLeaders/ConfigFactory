81eed1901461f851068eba148fcce355b8f6c65c
ArchLeaders <archparliament@gmail.com>
2023-06-16 14:52:45 -0700

    Update demo

--- src/ConfigFactory.Avalonia.Demo/Models/DemoConfig.cs
+++ src/ConfigFactory.Avalonia.Demo/Models/DemoConfig.cs
@@ -1,11 +1,45 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using ConfigFactory.Core;
+using ConfigFactory.Core.Attributes;
+using System.Collections.ObjectModel;
 
 namespace ConfigFactory.Avalonia.Demo.Models;
 
 public partial class DemoConfig : ConfigModule<DemoConfig>
 {
     [ObservableProperty]
-    [property: Core.Attributes.Config(Header = "Some Field", Description = "Extended (probably redundant) description of Some Field, a text-based configuration")]
+    [property: Config(
+        Header = "Some Field",
+        Description = "Extended (probably redundant) description of Some Field, a text-based configuration",
+        Category = "General",
+        Group = "Common")]
     private string _someField = string.Empty;
+
+    [ObservableProperty]
+    [property: DropdownConfig(
+        RuntimeItemsSourceMethodName = "GetThings")]
+    [property: Config(
+        Header = "Some Dropdown Field",
+        Description = "Extended (probably redundant) description of Some Field, a text-based configuration",
+        Category = "General",
+        Group = "Common")]
+    private string _someDropdownField = string.Empty;
+
+    [ObservableProperty]
+    [property: BrowserConfig(BrowserMode = BrowserMode.OpenFile)]
+    [property: Config(
+        Header = "Some Other Field",
+        Description = "Extended (probably redundant) description of Some Field, a text-based configuration",
+        Category = "General",
+        Group = "Less Common")]
+    private string _someOtherField = string.Empty;
+
+    public static ObservableCollection<string> GetThings()
+    {
+        return new() {
+            { "Entry One" },
+            { "Entry Two" },
+            { "Entry Three" },
+        };
+    }
 }
--- src/ConfigFactory.Avalonia.Demo/Views/MainWindow.axaml.cs
+++ src/ConfigFactory.Avalonia.Demo/Views/MainWindow.axaml.cs
@@ -1,5 +1,5 @@
 using Avalonia.Controls;
-using ConfigFactory.Avalonia.Controls;
+using ConfigFactory.Avalonia.Demo.Models;
 using ConfigFactory.Avalonia.Models;
 
 namespace ConfigFactory.Avalonia.Demo.Views;
@@ -8,5 +8,8 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        if (ConfigPage.DataContext is ConfigPageModel model) {
+            model.Append<DemoConfig>();
+        }
     }
 }
