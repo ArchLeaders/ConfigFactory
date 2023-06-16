509bd30498759f9bc3bd21619a07245f0a48bbb6
ArchLeaders <archparliament@gmail.com>
2023-06-16 12:08:24 -0700

    Stub Avalonia.ConfigFactory

--- src/ConfigFactory.Avalonia/ConfigFactory.cs
+++ src/ConfigFactory.Avalonia/ConfigFactory.cs
@@ -1,8 +1,21 @@
-﻿using ConfigFactory.Core.Component;
+﻿using ConfigFactory.Avalonia.Models;
+using ConfigFactory.Core;
 
 namespace ConfigFactory.Avalonia;
 
-public class ConfigFactory : IConfigFactory
+public static class ConfigFactory
 {
+    public static ConfigPageModel Build<T>() where T : ConfigModule<T>, new() => Build(ConfigModule<T>.Shared);
+    public static ConfigPageModel Build<T>(T module) where T : IConfigModule
+    {
+        ConfigPageModel configPageModel = new();
+        configPageModel.Append(module);
+        return configPageModel;
+    }
 
+    public static ConfigPageModel Append<T>(this ConfigPageModel configPageModel) where T : ConfigModule<T>, new() => Append(configPageModel, ConfigModule<T>.Shared);
+    public static ConfigPageModel Append<T>(this ConfigPageModel configPageModel, T module) where T : IConfigModule
+    {
+        return configPageModel;
+    }
 }
