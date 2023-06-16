383b91fb59af2f2ede85bc592a93cb6a7079d560
ArchLeaders <archparliament@gmail.com>
2023-06-15 22:50:32 -0700

    Stub ConfigAttribute

---
+++ src/ConfigFactory.Core/Attributes/ConfigAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace ConfigFactory.Core.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class ConfigAttribute : Attribute
+{
+    public required string Header { get; set; }
+    public required string Description { get; set; }
+}
