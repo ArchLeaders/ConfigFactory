a9433153b560ef0f669b4edc1eb17cfae84317ad
ArchLeaders <archparliament@gmail.com>
2023-06-16 14:50:01 -0700

    Impl TextControlBuilder

---
+++ src/ConfigFactory.Avalonia/Builders/TextControlBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Layout;
+using ConfigFactory.Avalonia.Generics;
+using ConfigFactory.Core;
+
+namespace ConfigFactory.Avalonia.Builders;
+
+public class TextControlBuilder : ControlBuilder<TextControlBuilder>
+{
+    public override object? Build(IConfigModule context, string propertyName)
+    {
+        return new TextBox {
+            DataContext = context,
+            VerticalAlignment = VerticalAlignment.Top,
+            [!TextBox.TextProperty] = new Binding(propertyName)
+        };
+    }
+
+    public override bool IsValid(object? value)
+    {
+        return value is string;
+    }
+}
