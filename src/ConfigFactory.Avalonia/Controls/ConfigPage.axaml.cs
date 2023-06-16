cce85f34f77f371d2e2e80e31590075db6752e40
ArchLeaders <archparliament@gmail.com>
2023-06-16 14:46:56 -0700

    Impl FocusDelegates elements

--- src/ConfigFactory.Avalonia/Controls/ConfigPage.axaml.cs
+++ src/ConfigFactory.Avalonia/Controls/ConfigPage.axaml.cs
@@ -6,5 +6,7 @@ public partial class ConfigPage : UserControl
     public ConfigPage()
     {
         InitializeComponent();
+        PART_FocusDelegate_1.PointerPressed += (s, e) => PART_FocusDelegate_1.Focus();
+        PART_FocusDelegate_2.PointerPressed += (s, e) => PART_FocusDelegate_2.Focus();
     }
 }
