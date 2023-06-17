745b8de4d580ced49409f7cfae7446bbeec9e810
ArchLeaders <archparliament@gmail.com>
2023-06-16 20:08:11 -0700

    Impl custom types in TextControlBuilder

--- src/ConfigFactory.Avalonia/Builders/TextControlBuilder.cs
+++ src/ConfigFactory.Avalonia/Builders/TextControlBuilder.cs
@@ -1,19 +1,50 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Data;
 using Avalonia.Layout;
 using ConfigFactory.Avalonia.Generics;
 using ConfigFactory.Core;
+using ConfigFactory.Core.Attributes;
+using System.Reflection;
 
 namespace ConfigFactory.Avalonia.Builders;
 
 public class TextControlBuilder : ControlBuilder<TextControlBuilder>
 {
-    public override object? Build(IConfigModule context, string propertyName)
+    public override object? Build(IConfigModule context, PropertyInfo propertyInfo)
     {
+        if (propertyInfo.GetCustomAttribute<BrowserConfigAttribute>() is BrowserConfigAttribute) {
+            return new StackPanel {
+                DataContext = context,
+                Orientation = Orientation.Horizontal,
+                Spacing = 5,
+                VerticalAlignment = VerticalAlignment.Top,
+                Children = {
+                    new TextBox {
+                        [!TextBox.TextProperty] = new Binding(propertyInfo.Name)
+                    },
+                    new Button {
+                        Width = 20,
+                        Height = 20,
+                        Content = "..."
+                    }
+                }
+            };
+        }
+        else if (propertyInfo.GetCustomAttribute<DropdownConfigAttribute>() is DropdownConfigAttribute dca) {
+            return new ComboBox {
+                DataContext = context,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                ItemsSource = dca.GetItemsSource(context),
+                VerticalAlignment = VerticalAlignment.Top,
+                [!SelectingItemsControl.SelectedItemProperty] = new Binding(propertyInfo.Name)
+            };
+        }
+
         return new TextBox {
             DataContext = context,
             VerticalAlignment = VerticalAlignment.Top,
-            [!TextBox.TextProperty] = new Binding(propertyName)
+            [!TextBox.TextProperty] = new Binding(propertyInfo.Name)
         };
     }
 
--- src/ConfigFactory.Core/Attributes/DropdownConfigAttribute.cs
+++ src/ConfigFactory.Core/Attributes/DropdownConfigAttribute.cs
@@ -1,4 +1,6 @@
-﻿namespace ConfigFactory.Core.Attributes;
+﻿using System.Collections;
+
+namespace ConfigFactory.Core.Attributes;
 
 [AttributeUsage(AttributeTargets.Property)]
 public class DropdownConfigAttribute : Attribute
@@ -10,6 +12,15 @@ public class DropdownConfigAttribute : Attribute
     /// class to return an <see cref="IEnumerable{T}"/> of source elements
     /// </summary>
     public string? RuntimeItemsSourceMethodName { get; set; }
+
+    public IEnumerable GetItemsSource(object? context)
+    {
+        if (RuntimeItemsSourceMethodName != null) {
+            return context?.GetType().GetMethod(RuntimeItemsSourceMethodName)?.Invoke(context, null) as IEnumerable ?? ItemsSource;
+        }
+
+        return ItemsSource;
+    }
 
     public DropdownConfigAttribute() { }
     public DropdownConfigAttribute(params string[] items)
