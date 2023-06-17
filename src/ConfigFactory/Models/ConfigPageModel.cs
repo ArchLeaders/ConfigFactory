b9869154c6c6ba4db2401f74b4d910a7254513c1
ArchLeaders <archparliament@gmail.com>
2023-06-17 13:43:47 -0700

    Document config page model

--- src/ConfigFactory/Models/ConfigPageModel.cs
+++ src/ConfigFactory/Models/ConfigPageModel.cs
@@ -6,39 +6,92 @@ namespace ConfigFactory.Models;
 
 public partial class ConfigPageModel : ObservableObject
 {
+    /// <summary>
+    /// The loaded <see cref="ConfigCategory"/> objects
+    /// </summary>
     [ObservableProperty]
     private ObservableCollection<ConfigCategory> _categories = new();
 
+    /// <summary>
+    /// The current selected <see cref="ConfigGroup"/>
+    /// </summary>
     [ObservableProperty]
     private ConfigGroup? _selectedGroup;
 
+    /// <summary>
+    /// Gets or sets the secondary button Content property<br/>
+    /// (Default: Save)
+    /// </summary>
     [ObservableProperty]
     private object? _primaryButtonContent = "Save";
 
+    /// <summary>
+    /// Gets or sets the secondary button Content property<br/>
+    /// (Default: Cancel)
+    /// </summary>
     [ObservableProperty]
     private object? _secondaryButtonContent = "Cancel";
 
+    /// <summary>
+    /// Gets or sets the primary button IsEnabeld property<br/>
+    /// (Default: True)
+    /// </summary>
     [ObservableProperty]
     private bool _primaryButtonIsEnabled = true;
 
+    /// <summary>
+    /// Gets or sets the secondary button IsEnabeld property<br/>
+    /// (Default: True)
+    /// </summary>
     [ObservableProperty]
     private bool _secondaryButtonIsEnabled = true;
 
+    /// <summary>
+    /// The event the runs when the primary button is clicked
+    /// </summary>
     public event Func<Task>? PrimaryButtonEvent;
+
+    /// <summary>
+    /// The event the runs when the secondary button is clicked
+    /// </summary>
     public event Func<Task>? SecondaryButtonEvent;
 
+    /// <summary>
+    /// The event the runs <b><i>after</i></b> the primary button is clicked
+    /// </summary>
+    public event Func<Task>? PrimaryButtonCompletedEvent;
+
+    /// <summary>
+    /// The event the runs <b><i>after</i></b> the secondary button is clicked
+    /// </summary>
+    public event Func<Task>? SecondaryButtonCompletedEvent;
+
+    /// <summary>
+    /// The primary relay command used by the PrimaryButton control<br/>
+    /// Executes the <see cref="PrimaryButtonEvent"/> and <see cref="PrimaryButtonCompletedEvent"/> events)
+    /// </summary>
+    /// <returns>The awaitable <see cref="Task"/></returns>
     [RelayCommand]
-    public Task PrimaryRelay()
+    public async Task PrimaryRelay()
     {
-        return PrimaryButtonEvent?.Invoke()
-            ?? Task.CompletedTask;
+        await (PrimaryButtonEvent?.Invoke()
+            ?? Task.CompletedTask);
+        await (PrimaryButtonCompletedEvent?.Invoke()
+            ?? Task.CompletedTask);
     }
 
+    /// <summary>
+    /// The primary relay command used by the PrimaryButton control<br/>
+    /// Executes the <see cref="SecondaryButtonEvent"/> and <see cref="SecondaryButtonCompletedEvent"/> events)
+    /// </summary>
+    /// <returns>The awaitable <see cref="Task"/></returns>
     [RelayCommand]
-    public Task SecondaryRelay()
+    public async Task SecondaryRelay()
     {
-        return SecondaryButtonEvent?.Invoke()
-            ?? Task.CompletedTask;
+        await(SecondaryButtonEvent?.Invoke()
+            ?? Task.CompletedTask);
+        await(SecondaryButtonCompletedEvent?.Invoke()
+            ?? Task.CompletedTask);
     }
 
     partial void OnSelectedGroupChanged(ConfigGroup? oldValue, ConfigGroup? newValue)
