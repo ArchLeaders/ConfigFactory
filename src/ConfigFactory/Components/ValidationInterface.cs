15ee7c0a7e1e2b4accd0841aca29e3413184ab8f
ArchLeaders <archparliament@gmail.com>
2023-07-11 09:29:36 -0700

    Clarify variable names

--- src/ConfigFactory/Components/ValidationInterface.cs
+++ src/ConfigFactory/Components/ValidationInterface.cs
@@ -7,18 +7,18 @@ namespace ConfigFactory.Components;
 
 internal class ValidationInterface : IValidationInterface
 {
-    private readonly ConfigPageModel _configPageModel;
+    private readonly ConfigPageModel _configPage;
 
     public ValidationInterface(ConfigPageModel configPageModel)
     {
-        _configPageModel = configPageModel;
+        _configPage = configPageModel;
     }
 
     public void SetValidationColor(PropertyInfo property, string hexColorCode)
     {
         if (property.GetCustomAttribute<ConfigAttribute>() is ConfigAttribute attribute) {
             string key = $"{attribute.Category}/{attribute.Group}/{property.Name}";
-            if (_configPageModel.ItemsMap.TryGetValue(key, out ConfigItem? configItem)) {
+            if (_configPage.ItemsMap.TryGetValue(key, out ConfigItem? configItem)) {
                 configItem.ValidationColor = hexColorCode;
             }
         }
